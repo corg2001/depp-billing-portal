7beca239c09f4921f046ed05c8a7c2c32b13956d
Pavani Surarapu <pavani.surarapu@directenergy.com>
2019-08-13 10:24:20 -0500

    Layout navigation

--- src/app/layout/footer/footer-links/footer-links.component.ts
+++ src/app/layout/footer/footer-links/footer-links.component.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+enum FooterLinks {
+  about = 'https://www.hwahomewarranty.com/about',
+  values = 'https://www.hwahomewarranty.com/about/our-values',
+  news = 'https://www.hwahomewarranty.com/news',
+  policy = 'https://www.hwahomewarranty.com/privacy',
+  terms = 'https://www.hwahomewarranty.com/terms-of-use',
+  contact = 'https://www.hwahomewarranty.com/contact-us',
+  faqs = 'https://www.hwahomewarranty.com/homeowners/frequently-asked-questions'
+}
+
 @Component({
   selector: 'app-footer-links',
   templateUrl: './footer-links.component.html',
   styleUrls: ['./footer-links.component.scss']
 })
 export class FooterLinksComponent implements OnInit {
+  public footerLinks = FooterLinks;
   constructor() { }
 
   ngOnInit() {
