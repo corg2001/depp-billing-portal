37426b4895ab99bd61cedbf6044981fb9dd3e0ec
Agiet Tanyingu <agiet.tonyingo@directenergy.com>
2019-08-23 12:47:48 -0500

    created claim notification feature

---
+++ src/app/account/claim-management/claim-summary/claim-notification/claim-notification.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ClaimNotificationComponent } from './claim-notification.component';
+
+describe('ClaimNotificationComponent', () => {
+  let component: ClaimNotificationComponent;
+  let fixture: ComponentFixture<ClaimNotificationComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ClaimNotificationComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ClaimNotificationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
