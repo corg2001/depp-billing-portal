3cf1ce81d213acb8f541b9ca65008692a6ed78f1
Agiet Tanyingu <agiet.tonyingo@directenergy.com>
2019-08-23 10:26:19 -0500

    created login enum and implemented

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -8,6 +8,7 @@ import { UserService } from '../user.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { PrivacyPolicyComponent } from '../privacy-policy/privacy-policy.component';
 import { TermsOfUseComponent } from '../terms-of-use/terms-of-use.component';
+import { LoginError } from './model/enums/login-error.enums';
 
 @Component({
   selector: 'app-login',
@@ -23,6 +24,7 @@ export class LoginComponent implements OnInit {
   public responseErrorMessage: string;
   private errorMessage: string =
     'Sign in failed. Please re-enter your password to try again. If you need help, give Contractor Relations a call at 1-888-888-8888';
+    private _
 
   // TODO: Pull this information from teh config
   public siblingPortals: any = {
@@ -39,8 +41,8 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
     this.buildForm();
     this.dataSubject.subscribe((data: any) => {
-      if (data.error.message === 'user authentication failed') {
-        data.error.message = this.errorMessage;
+      if (data.error.message === LoginError.message || LoginError.credentials) {
+        data.error.message = LoginError.message;
       }
       this.responseErrorMessage = data.error.message;
     });
