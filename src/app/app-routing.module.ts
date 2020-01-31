53c390252527ebc65b96d7fc048e3aa25ce96adf
kartheek moturu <kartheek.moturu@directenergy.com>
2020-01-30 21:47:11 -0600

    Revert "Merge branch 'maintence-page' into 'development'"
    This reverts merge request !157

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TestComponent } from './test/test.component';
-import { environment } from '../environments/environment';
-import { MaintenancePageComponent } from './maintenance-page/maintenance-page.component';
-
-let maintenancePath: string = 'maintenance';
-environment.core.maintenance.active
-  ? (maintenancePath = '**')
-  : (maintenancePath = 'maintenance');
 
 const routes: Routes = [
-  {
-    path: maintenancePath,
-    pathMatch: 'full',
-    component: MaintenancePageComponent
-  },
   {
     path: 'test',
     component: TestComponent,
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,10 @@ import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { ModalComponent } from './shared/modal/modal.component';
 import { TestComponent } from './test/test.component';
-import { MaintenancePageComponent } from './maintenance-page/maintenance-page.component';
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
-    MaintenancePageComponent
   ],
   imports: [
     BrowserModule,
