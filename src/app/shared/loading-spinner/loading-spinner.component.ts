497eb12acfd93a60b3bc2b363989084f9bc2b789
Agiet Tanyingu <agiet.tonyingo@directenergy.com>
2020-05-29 11:58:04 -0500

    update method that redirects user to auth invoice to open window with new url once the api returns the url

--- src/app/shared/loading-spinner/loading-spinner.component.ts
+++ src/app/shared/loading-spinner/loading-spinner.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-loading-spinner',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./loading-spinner.component.scss']
 })
 export class LoadingSpinnerComponent implements OnInit {
-
+  @Input() public message?: string;
+  @Input() public addPadding = true;
   constructor() { }
 
   ngOnInit() {
