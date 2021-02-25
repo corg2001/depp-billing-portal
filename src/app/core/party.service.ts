bf8c1db8076cbdc1abc754dff0612824231c3688
Santhosh Raju <santhosh.raju@directenergy.com>
2021-02-25 02:21:53 +0000

    After hf1 deployment - UI json format change
    UI issue fix for water heater form type.
    
    Update diagnosis-form.component.ts
    
    Modified alignment review comments

--- src/app/core/party.service.ts
+++ src/app/core/party.service.ts
@@ -39,6 +39,8 @@ export class PartyService {
     this._http.get(environment.partyDetailsUrl).subscribe(
       (responseData: PartyDetailsPayloadInterface) => {
         this.getPartyDetailsSuccessHandler(subscription, responseData);
+        if(responseData.associations._association.length > 0)
+          sessionStorage.setItem('company_info',JSON.stringify(responseData.associations._association[0].company_info));
       },
       (responseError: Observable<HttpErrorResponse>) => {
         this.getPartyDetailsFailureHandler(subscription, responseError);
