2c09ccc45bf157511f64929e1b8c97ea3e142b85
Agiet Tanyingu <agiet.tonyingo@directenergy.com>
2021-11-22 18:08:20 +0000

    Decommission Logic  update: removed testing logic

--- src/app/account/claim-management/diagnosis/diagnosis-select-modal/diagnosis-select-modal.component.ts
+++ src/app/account/claim-management/diagnosis/diagnosis-select-modal/diagnosis-select-modal.component.ts
@@ -48,7 +48,6 @@ export class DiagnosisSelectModalComponent implements OnInit {
 
   public isDiagnosisFormEnabled(): boolean {
     const launchDate = (moment(environment.aspirePhase4releaseDate));
-    // return launchDate < moment().add(1, 'M');
-    return launchDate  === moment();
+    return launchDate < moment().add(1, 'M');
   }
 }
