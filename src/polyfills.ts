610cdaaca10983618c493abb3029c4866cafdb47
agiet.tonyingo <agiet.tonyingo@nrg.com>
2025-12-12 14:14:51 -0600

    remove codelyzer and updated zone.js

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -57,7 +57,7 @@
  */
 import '@angular/localize/init';
 
-import 'zone.js/dist/zone';  // Included with Angular CLI.
+import 'zone.js';  // Included with Angular CLI.
 
 
 /***************************************************************************************************
